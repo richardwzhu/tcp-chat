35dd49dbbe8b43f6eeab1742678dc072e1ff5d3e
Richard Zhu <richardzhu@richards-mbp.mynetworksettings.com>
2023-11-11 22:06:00 -0500

    Add client and server main

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
 # TCP Chat Server and Client
 
+## Introduction
 A simple chat server and client that communicates over TCP.
 
 ## Project Structure
 
-```
+```plaintext
 tcp-chat/
 │
 ├── src/                    # Source files
@@ -31,3 +32,29 @@ tcp-chat/
 ├── CMakeLists.txt          # CMake project file
 └── README.md               # Project overview and guide
 ```
+
+## Installation
+
+#### Prerequisites:
+* A C++ compiler (e.g., g++ or clang)
+
+#### Compilation:
+Navigate to the respective directories (`src/server/` and `src/client/)` and run the following commands to compile the server and client:
+
+For the server:
+```bash
+g++ -o server main.cpp
+```
+
+For the client:
+```bash
+g++ -o client main.cpp
+```
+
+## Usage
+Start the Server:
+Navigate to the server's directory and run `./server`.
+The server will start and listen for incoming connections.
+Run the Client:
+In a new terminal, navigate to the client's directory and run `./client`.
+The client will connect to the server and you can start chatting.
---
+++ src/server/main.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <cstring>
+#include <unistd.h>
+
+int main() {
+    int server_fd, new_socket;
+    struct sockaddr_in address;
+    int addrlen = sizeof(address);
+    char buffer[1024] = {0};
+
+    // Creating socket file descriptor
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+        perror("socket failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Bind the socket to the IP / port
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(8080);
+
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
+        perror("bind failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Server listen
+    if (listen(server_fd, 3) < 0) {
+        perror("listen");
+        exit(EXIT_FAILURE);
+    }
+
+    // Accept incoming connection
+    if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen)) < 0) {
+        perror("accept");
+        exit(EXIT_FAILURE);
+    }
+
+    // Read message from client
+    read(new_socket, buffer, 1024);
+    std::cout << "Message from client: " << buffer << std::endl;
+
+    // Send a response
+    const char *hello = "Hello from server";
+    send(new_socket, hello, strlen(hello), 0);
+    std::cout << "Hello message sent\n";
+
+    // Close the socket
+    close(new_socket);
+
+    return 0;
+}
